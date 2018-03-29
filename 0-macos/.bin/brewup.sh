3d5893cc232ffb4fcafe4b7fd92065432e8c4ab7
Christopher Allen <ChristopherA@LifeWithAlacrity.com>
2018-03-29 11:48:31 -0700

    Added brewup to ~/.bin to update brew

---
+++ 0-macos/.bin/brewup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# from https://apple.stackexchange.com/questions/206465/auto-updating-homebrew
+
+brew=/usr/local/bin/brew
+logger=/usr/bin/logger
+
+$brew update 2>&1  | $logger -t brewup.update
+$brew upgrade 2>&1 | $logger -t brewup.upgrade
+$brew cleanup 2>&1 | $logger -t brewup.cleanup
